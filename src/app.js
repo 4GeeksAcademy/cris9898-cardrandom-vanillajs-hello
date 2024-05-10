a038f03d6b053403a4dd56954a13abc505f2d99d
Cris9898 <157153132+Cris9898@users.noreply.github.com>
2024-05-10 20:12:12 +0000

    Second Commit

--- src/app.js
+++ src/app.js
@@ -9,33 +9,59 @@ window.onload = function() {
   const figures = ["♦", "♥", "♠", "♣"];
   const cardNumber = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "K", "Q", "A"];
 
-  const topElement = document.getElementById("topFigure");
-  const cardNumberElement = document.getElementById("randomNumber");
-  const bottomElement = document.getElementById("bottomFigure");
+  const appElement = document.getElementById("app");
 
-  const randomFigure = figures[Math.floor(Math.random() * figures.length)];
-  const randomNumberCard =
-    cardNumber[Math.floor(Math.random() * cardNumber.length)];
+  const createCardElement = document.getElementById("createCard");
 
-  const topPCreateElement = document.createElement("div");
-  const bottomPCreateElement = document.createElement("div");
-  const numberCreateElement = document.createElement("div");
+  createCard;
+  createCardElement.addEventListener("click", createCard);
 
-  console.log(randomFigure);
+  setInterval(createCard, 10000);
 
-  if (randomFigure === "♥" || randomFigure === "♦") {
-    topPCreateElement.classList.add("red");
-    bottomElement.classList.add("red");
-  } else {
-    topPCreateElement.classList.add("black");
-    bottomElement.classList.add("black");
+  //Funcion para crear Cards
+  function createCard() {
+    const randomFigure = figures[Math.floor(Math.random() * figures.length)];
+    const colorFigure =
+      randomFigure === "♦" || randomFigure === "♥" ? "red" : "black";
+    //el div de carta
+    const cardElement = document.createElement("div");
+    cardElement.classList.add("card");
+    cardElement.style.width = "18rem";
+    appElement.appendChild(cardElement);
+
+    //El top de la carta
+
+    const cardTopElement = document.createElement("div");
+    cardTopElement.classList.add("card-body", colorFigure);
+    cardTopElement.id = "topFigure";
+    cardTopElement.innerHTML = randomFigure;
+    cardElement.appendChild(cardTopElement);
+
+    //Va el numero de la carta en medio de la carta
+
+    const cardNumberElement = document.createElement("div");
+    cardNumberElement.classList.add(
+      "d-flex",
+      "justify-content-center",
+      "number"
+    );
+    cardNumberElement.id = "randomNumber";
+    cardNumberElement.innerHTML =
+      cardNumber[Math.floor(Math.random() * cardNumber.length)];
+    cardElement.appendChild(cardNumberElement);
+
+    //La figura la final de la carta
+
+    const cardBottomElement = document.createElement("div");
+    cardBottomElement.classList.add(
+      "d-flex",
+      "justify-content-end",
+      "reverse",
+      "m-3",
+      colorFigure
+    );
+    cardNumberElement.id = "bottomFigure";
+    cardBottomElement.innerHTML = randomFigure;
+    cardElement.appendChild(cardBottomElement);
   }
-
-  topPCreateElement.innerHTML += randomFigure;
-  bottomPCreateElement.innerHTML += randomFigure;
-  numberCreateElement.innerHTML += randomNumberCard;
-
-  topElement.appendChild(topPCreateElement);
-  cardNumberElement.appendChild(numberCreateElement);
-  bottomElement.appendChild(bottomPCreateElement);
 };
